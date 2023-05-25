186586ca64fba547ee1f83aec4db1215c506ba96
Dotnet Format Bot (by YeJun, Jung) <yejun614@naver.com>
2023-05-25 00:49:04 +0000

    [Style] Auto dotnet format

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Hello, World!");
-  Console.WriteLine("Hello, World!");
-    Console.WriteLine("Hello, World!");
-      Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
