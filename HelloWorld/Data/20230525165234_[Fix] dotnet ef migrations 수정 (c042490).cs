623b55c7fbb59f0b5b96b52f26df59cf3e12f6b2
Dotnet Migration Bot (by YeJun, Jung) <yejun614@naver.com>
2023-05-25 16:52:42 +0000

    [Chore] Auto EntityFramework Migrations

---
+++ HelloWorld/Data/20230525165234_[Fix] dotnet ef migrations 수정 (c042490).cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HelloWorld.Data
+{
+    /// <inheritdoc />
+    public partial class Fixdotnetefmigrations수정c042490 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
