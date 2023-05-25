62602353ce0e470cb7b7d503eb010b9f064295df
Dotnet Migration Bot (by YeJun, Jung) <yejun614@naver.com>
2023-05-25 16:59:43 +0000

    [Chore] Auto EntityFramework Migrations

---
+++ HelloWorld/Data/20230525165937_[Fix] hello message (855caac).cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HelloWorld.Data
+{
+    /// <inheritdoc />
+    public partial class Fixhellomessage855caac : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
